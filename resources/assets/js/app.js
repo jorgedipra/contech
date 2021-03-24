b8081f8b4629c0f0e772f2d766fe304ab36e0905
jorgedipra <jorgedipra@gmail.com>
2021-03-24 17:57:07 -0500

    ajuste de entorno

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,21 +1,19 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+library.add(faUserSecret, fas, fab)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.config.productionTip = false
+
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('home', require('./components/HomeComponent.vue'));
 
 const app = new Vue({
     el: '#app'
